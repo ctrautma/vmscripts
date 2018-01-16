954f71f0fdb5174fcefa8301ed76859aba4f7c15
Christian Trautman <ctrautma@ctrautma.bos.csb>
2018-01-16 15:01:45 -0500

    Adding 1611-4 to rpm downloads

--- setup_rpms.sh
+++ setup_rpms.sh
@@ -25,7 +25,7 @@ shift $(($OPTIND - 1))
 
 yum install libibverbs -y
 
-mkdir -P /root/dpdkrpms/1705 /root/dpdkrpms/1611-2 /root/tuned/28 /root/tuned/27 /root/dpdkrpms/1711
+mkdir -P /root/dpdkrpms/1705 /root/dpdkrpms/1611-2 /root/tuned/28 /root/tuned/27 /root/dpdkrpms/1711 /root/dpdkrpms/1611-4
 SERVER="download-node-02.eng.bos.redhat.com"
 wget http://$SERVER/brewroot/packages/tuned/2.7.1/5.el7fdb/noarch/tuned-2.7.1-5.el7fdb.noarch.rpm -P /root/tuned/27/.
 wget http://$SERVER/brewroot/packages/tuned/2.7.1/5.el7fdb/noarch/tuned-profiles-cpu-partitioning-2.7.1-5.el7fdb.noarch.rpm -P /root/tuned/27/.
@@ -39,6 +39,8 @@ wget http://$SERVER/brewroot/packages/dpdk/16.11.2/4.el7/x86_64/dpdk-16.11.2-4.e
 wget http://$SERVER/brewroot/packages/dpdk/16.11.2/4.el7/x86_64/dpdk-tools-16.11.2-4.el7.x86_64.rpm -P /root/dpdkrpms/1611-2/.
 wget http://$SERVER/brewroot/packages/dpdk/17.11/2.el7fdb/x86_64/dpdk-17.11-2.el7fdb.x86_64.rpm -P /root/dpdkrpms/1711/.
 wget http://$SERVER/brewroot/packages/dpdk/17.11/2.el7fdb/x86_64/dpdk-tools-17.11-2.el7fdb.x86_64.rpm -P /root/dpdkrpms/1711/.
+wget http://$SERVER/brewroot/packages/dpdk/16.11/4.el7fdp/x86_64/dpdk-16.11-4.el7fdp.x86_64.rpm -P /root/dpdkrpms/1611-4/.
+wget http://$SERVER/brewroot/packages/dpdk/16.11/4.el7fdp/x86_64/dpdk-tools-16.11-4.el7fdp.x86_64.rpm -P /root/dpdkrpms/1611-4/.
 
 # Detect OS name and version from systemd based os-release file
 . /etc/os-release
