822cce5f963bc60d90354f60c7b5e2abb8b131f2
Christian Trautman <ctrautma@ctrautma.bos.csb>
2018-02-19 09:31:23 -0500

    Updating dpdk 1711 to altest

--- setup_rpms.sh
+++ setup_rpms.sh
@@ -44,6 +44,11 @@ BUILD_DIR="${IPERF_FILE%.tar.gz}"
 cd ${BUILD_DIR}
 ./configure && make && make install
 
+rm -f ${IPERF_FILE}
+rm -Rf IPERF*
+rm -f ${netperf}.tar.gz
+rm -Rf netperf*
+
 mkdir -P /root/dpdkrpms/1705 /root/dpdkrpms/1611-2 /root/tuned/28 /root/tuned/27 /root/dpdkrpms/1711 /root/dpdkrpms/1611-4
 SERVER="download-node-02.eng.bos.redhat.com"
 wget http://$SERVER/brewroot/packages/tuned/2.7.1/5.el7fdb/noarch/tuned-2.7.1-5.el7fdb.noarch.rpm -P /root/tuned/27/.
@@ -56,8 +61,8 @@ wget http://$SERVER/brewroot/packages/dpdk/17.05/2.el7fdb/x86_64/dpdk-17.05-2.el
 wget http://$SERVER/brewroot/packages/dpdk/17.05/2.el7fdb/x86_64/dpdk-tools-17.05-2.el7fdb.x86_64.rpm -P /root/dpdkrpms/1705/.
 wget http://$SERVER/brewroot/packages/dpdk/16.11.2/4.el7/x86_64/dpdk-16.11.2-4.el7.x86_64.rpm -P /root/dpdkrpms/1611-2/.
 wget http://$SERVER/brewroot/packages/dpdk/16.11.2/4.el7/x86_64/dpdk-tools-16.11.2-4.el7.x86_64.rpm -P /root/dpdkrpms/1611-2/.
-wget http://$SERVER/brewroot/packages/dpdk/17.11/2.el7fdb/x86_64/dpdk-17.11-2.el7fdb.x86_64.rpm -P /root/dpdkrpms/1711/.
-wget http://$SERVER/brewroot/packages/dpdk/17.11/2.el7fdb/x86_64/dpdk-tools-17.11-2.el7fdb.x86_64.rpm -P /root/dpdkrpms/1711/.
+wget http://$SERVER/brewroot/packages/dpdk/17.11/6.el7fdb/x86_64/dpdk-17.11-6.el7fdb.x86_64.rpm -P /root/dpdkrpms/1711/.
+wget http://$SERVER/brewroot/packages/dpdk/17.11/6.el7fdb/x86_64/dpdk-tools-17.11-6.el7fdb.x86_64.rpm -P /root/dpdkrpms/1711/.
 wget http://$SERVER/brewroot/packages/dpdk/16.11/4.el7fdp/x86_64/dpdk-16.11-4.el7fdp.x86_64.rpm -P /root/dpdkrpms/1611-4/.
 wget http://$SERVER/brewroot/packages/dpdk/16.11/4.el7fdp/x86_64/dpdk-tools-16.11-4.el7fdp.x86_64.rpm -P /root/dpdkrpms/1611-4/.
 
