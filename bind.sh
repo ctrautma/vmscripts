4d89e606fb8926f1aa1f1e6b8723882ab1e8d508
Christian Trautman <ctrautma@ctrautma.bos.csb>
2018-05-24 16:52:48 -0400

    Adding in argument options to bind.sh

--- bind.sh
+++ bind.sh
@@ -5,6 +5,15 @@ set -x
 NIC1=eth1
 NIC2=eth2
 
+if [[ $# -eq 2 ]]; then
+    NIC1=$1
+    NIC2=$2
+else if [[ $# -ne 0 ]]; then
+    echo "Please pass Zero or Two parameters."
+    exit 1
+fi
+fi
+
 NIC1_PCI_ADDR=`ethtool -i $NIC1 | grep -Eo '[0-9]+:[0-9]+:[0-9]+\.[0-9]+'`
 NIC2_PCI_ADDR=`ethtool -i $NIC2 | grep -Eo '[0-9]+:[0-9]+:[0-9]+\.[0-9]+'`
 
@@ -19,4 +28,5 @@ dpdk-devbind -b vfio-pci $NIC1_PCI_ADDR
 sleep 3
 dpdk-devbind -b vfio-pci $NIC2_PCI_ADDR
 sleep 3
+
 
